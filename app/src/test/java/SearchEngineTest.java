2f2bcff715b734502f4d00c4e1b2a42ba086e2df
DorofeevDaniil <dorofeefdi@gmail.com>
2025-02-27 15:47:40 +0300

    fixing sonarlight issues v10

--- app/src/test/java/SearchEngineTest.java
+++ app/src/test/java/SearchEngineTest.java
@@ -9,23 +9,23 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SearchEngineTest {
-  public static final String DOC1 = "I can't shoot straight unless I've had a pint! Pour me!";
-  public static final String DOC2 = "Don't shoot shoot shoot that thing at me.";
-  public static final String DOC3 = "I'm your shooter. It's me.";
-  public static final String DOC4 = "shoot me, shoot me!";
+    public static final String DOC1 = "I can't shoot straight unless I've had a pint! Pour me!";
+    public static final String DOC2 = "Don't shoot shoot shoot that thing at me.";
+    public static final String DOC3 = "I'm your shooter. It's me.";
+    public static final String DOC4 = "shoot me, shoot me!";
 
-  public static final List<Map<String, String>> DOCS = List.of(
-    Map.of("id", "doc1", "text", DOC1),
-    Map.of("id", "doc2", "text", DOC2),
-    Map.of("id", "doc3", "text", DOC3),
-    Map.of("id", "doc4", "text", DOC4)
-  );
+    public static final List<Map<String, String>> DOCS = List.of(
+      Map.of("id", "doc1", "text", DOC1),
+      Map.of("id", "doc2", "text", DOC2),
+      Map.of("id", "doc3", "text", DOC3),
+      Map.of("id", "doc4", "text", DOC4)
+    );
 
-  @Test
-  void searchOneWordTest() {
-    List<String> result = SearchEngine.search(DOCS, "pint");
-    List<String> correct = List.of("doc1");
+    @Test
+    void searchOneWordTest() {
+      List<String> result = SearchEngine.search(DOCS, "pint");
+      List<String> correct = List.of("doc1");
 
-    assertThat(result).isEqualTo(correct);
-  }
+      assertThat(result).isEqualTo(correct);
+    }
 }
